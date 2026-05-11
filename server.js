eedc0f7a7f2a1b25ab3d557e0e31a174d713de9e
aluis <aluis@microshopnet.net>
2026-05-11 13:51:41 +0200

    feat: add 301 redirect for microshop.info

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect microshop.info to www.microshopnet.net
+app.use((req, res, next) => {
+  const host = req.hostname;
+  if (host === 'microshop.info' || host === 'www.microshop.info') {
+    return res.redirect(301, 'https://www.microshopnet.net' + req.originalUrl);
+  }
+  next();
+});
+
 // Serve static files from the project root
 app.use(express.static(path.join(__dirname), {
   index: 'index.html',
